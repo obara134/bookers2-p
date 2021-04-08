15a0e0da52c2d9b349947a35a98803d0f845cf87
obara134 <tyobara@icloud.com>
2021-04-08 08:00:20 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
   get 'about' => 'homes#about'
+  resources :users, only: [:index, :show]
   
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
