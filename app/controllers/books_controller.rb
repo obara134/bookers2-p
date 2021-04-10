c686ed251ae4c2ffdfbdbcb9f88089338f71c26e
obara134 <tyobara@icloud.com>
2021-04-10 10:25:16 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,37 @@
 class BooksController < ApplicationController
-  
+
   def index
+    @book = Book.new
+    @user = User.find(current_user[:id])
+    @books = Book.all
   end
-  
+
   def create
+    @book=Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+
   end
-  
+
   def show
-    
+
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
-  
 
+  praivate
   
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
