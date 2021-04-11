4e1b1c1bde7e67e4865b5e178289bb1d0bebe409
obara134 <tyobara@icloud.com>
2021-04-11 16:49:40 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :book_edit_check,only: [:edit]
 
   def index
     @book = Book.new
@@ -20,9 +22,10 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = User.find(current_user[:id])
-    @new_book = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(@book.user.id)
+    @new_book = Book.new
+
   end
 
   def edit
@@ -33,7 +36,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.update(book_params)
     if @book.save
-      flash[:notice] = "You have  book successfully."
+      flash[:notice] = "You have updted book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -50,6 +53,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def book_edit_check
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
 
