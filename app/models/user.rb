4c3aa23b277a2c4c7cdb8714a3e67ccd830b64b2
obara134 <tyobara@icloud.com>
2021-04-19 17:22:09 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ApplicationRecord
 
   attachment :profile_image
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
-
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
-
+  get '/search', to: 'search#search'
 end
